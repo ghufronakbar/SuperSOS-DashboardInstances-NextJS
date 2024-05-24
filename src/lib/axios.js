1a16b7fb6f9a643af3bd9a83e84987967457e5a9
ghufron akbar <alan.040219.gam@gmail.com>
2024-05-25 04:27:31 +0700

    NGROK CONFIG

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,5 +2,16 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}/api/instance`
-})
+});
 
+if (!process.env.NEXT_PUBLIC_BASE_URL.includes("localhost")) {
+    // Add a request interceptor
+    axiosInstance.interceptors.request.use(function (config) {
+        // Add the ngrok-skip-browser-warning header to the request
+        config.headers['ngrok-skip-browser-warning'] = 'true';        
+        return config;
+    }, function (error) {
+        // Do something with request error        
+        return Promise.reject(error);
+    });
+}
